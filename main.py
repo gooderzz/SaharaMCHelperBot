78887afc25624d41a8d6b82d32413d1bae46f142
gooderzz <william.goodwin@mail.com>
2020-11-03 00:49:34 +0000

    Ability to do !apply

--- main.py
+++ main.py
@@ -5,7 +5,13 @@ import os
 from dotenv import load_dotenv
 
 # Discord Imports
+import discord
 from discord.ext import commands
+
+intents = discord.Intents.all()
+intents.members = True
+
+client = discord.Client()
 
 # env loads
 load_dotenv()
@@ -13,7 +19,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
 # bot prefix
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(intents=intents, command_prefix='!', case_insensitive=True)
 
 
 # bot events
@@ -39,7 +45,7 @@ async def rules(ctx):
 
 
 @bot.command(name='price', help='responds with the price of an item')
-async def price(ctx, item):
+async def price(ctx, *, item):
     print(item)
     with open('sd_prices.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
@@ -47,12 +53,18 @@ async def price(ctx, item):
         item = (item.capitalize())
         print(item)
         for row in csv_reader:
-            if row[0] == item:
+            if row[0].capitalize() == item:
                 print(f'{row[0]} costs {row[1]} {row[2]} per {int(row[3])/64} stacks.')
                 cost = (f'{row[0]} costs {row[1]} {row[2]} per {int(row[3])/64} stacks.')
 
     await ctx.send(cost)
 
+@bot.command(name='apply', help='apply to join the community', pass_context=True)
+async def apply(ctx):
+    me = ctx.author
+    print(me, 'is applying')
+    application_message = 'Hello!'
+    await me.send(embed=embedVar)
 
 # bot errors
 @bot.event
@@ -61,5 +73,20 @@ async def on_command_error(ctx, error):
         await ctx.send('You do not have the correct role for this command.')
 
 
+#Application Creation
+embedVar = discord.Embed(title="Apply For SaharaMC Whitelist", description="This is the application for SaharaMC whitelist", color=0x00ff00)
+embedVar.add_field(name="What is your Discord Name?", value="Discord Name", inline=True)
+embedVar.add_field(name="What is your minecraft in game name?", value="Minecraft Name", inline=True)
+embedVar.add_field(name="What is your age?", value="Age", inline=True)
+embedVar.add_field(name="How many hours a day do you play Minecraft?", value="Hours", inline=True)
+embedVar.add_field(name="What does a community mean to you?", value="Community", inline=True)
+embedVar.add_field(name="What is your favourite part of Minecraft and what do you look forward to doing the most if accepted?", value="Favourite part of Minecraft", inline=True)
+embedVar.add_field(name="Please explain a little bit about yourself. Maybe you have something unique that you would like to share. (3-4 sentences minimum please)", value="A little about yourself", inline=True)
+embedVar.add_field(name="What will you bring to the community? if you have any great builds you can post a link here. (3-4 sentences minimum please)", value="What will you bring", inline=True)
+embedVar.add_field(name="How did you hear about us? If from a website, please enter the website name.", value="Hear about us", inline=True)
+embedVar.add_field(name="You do understand SaharaMC is built on trust and that is why we emphasize the importance of the rules. Have you completely read all of the rules? Enter yes or no?", value="Acceptance", inline=True)
+embedVar.add_field(name="What is SaharaMC's golden rule?", value="Golden Rule", inline=True)
+
 # bot run
 bot.run(TOKEN)
+
